28893581f2d12bdab9be02f42f6389eb01b1043f
bool_tbb <247687157@qq.com>
2020-07-29 12:42:56 +0800

    leetcode

---
+++ bert.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding=utf-8
+import torch
+from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
+
+# OPTIONAL: if you want to have more information on what's happening, activate the logger as follows
+import logging
+logging.basicConfig(level=logging.INFO)
+
+#import matplotlib.pyplot as plt
+#% matplotlib inline
+
+# Load pre-trained model tokenizer (vocabulary)
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+text = "Here is the sentence I want embeddings for."
+marked_text = "[CLS] " + text + " [SEP]"
+
+# Tokenize our sentence with the BERT tokenizer.
+tokenized_text = tokenizer.tokenize(marked_text)
+
+# Print out the tokens.
+print (tokenized_text)
+
+# Define a new example sentence with multiple meanings of the word "bank"
+text = "After stealing money from the bank vault, the bank robber was seen " \
+       "fishing on the Mississippi river bank."
+
+# Add the special tokens.
+marked_text = "[CLS] " + text + " [SEP]"
+
+# Split the sentence into tokens.
+tokenized_text = tokenizer.tokenize(marked_text)
+
+# Map the token strings to their vocabulary indeces.
+indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+
+# Display the words with their indeces.
+for tup in zip(tokenized_text, indexed_tokens):
+    print('{:<12} {:>6,}'.format(tup[0], tup[1]))
+
+
+# Mark each of the 22 tokens as belonging to sentence "1".
+segments_ids = [1] * len(tokenized_text)
+
+print (segments_ids)
+
+# Convert inputs to PyTorch tensors
+tokens_tensor = torch.tensor([indexed_tokens])
+segments_tensors = torch.tensor([segments_ids])
+
+# Load pre-trained model (weights)
+model = BertModel.from_pretrained('bert-base-uncased')
+
+# Put the model in "evaluation" mode, meaning feed-forward operation.
+model.eval()
+
+
+# Predict hidden states features for each layer
+with torch.no_grad():
+    encoded_layers, _ = model(tokens_tensor, segments_tensors)
+
